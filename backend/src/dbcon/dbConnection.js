f7c756465b91e52b8d9a33aa3886c6bd2d53214f
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-24 02:28:25 +0530

    added js to provider

--- backend/src/dbcon/dbConnection.js
+++ backend/src/dbcon/dbConnection.js
@@ -1,7 +1,8 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 // import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from '../generated/client/index.js';
+// import { PrismaClient } from '../generated/client/index.js';
+import { PrismaClient } from '@prisma/client';
 // import { PrismaClient } from '../../generated/prisma/client.ts';
 // Grab the pooled port 6543 connection for your API traffic
 // Fallback to local DATABASE_URL for local development
