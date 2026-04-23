61a7e473d980b86fcc85dac42a7a6011ca46d6ac
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-24 02:20:33 +0530

    changed Prismaclient path into src/generated

--- backend/src/dbcon/dbConnection.js
+++ backend/src/dbcon/dbConnection.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
-
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/client/index.js';
 // Grab the pooled port 6543 connection for your API traffic
 // Fallback to local DATABASE_URL for local development
 const connectionString = process.env.POOLED_URL || process.env.DATABASE_URL;
