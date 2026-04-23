532f5c955687074a3fe60bea63ca128f98a731c7
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-24 01:51:27 +0530

    added new pool code

--- backend/src/dbcon/dbConnection.js
+++ backend/src/dbcon/dbConnection.js
@@ -1,10 +1,15 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
-const connectionString = process.env.DIRECT_URL;
+// Grab the pooled port 6543 connection for your API traffic
+// Fallback to local DATABASE_URL for local development
+const connectionString = process.env.POOLED_URL || process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString: connectionString });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// Prisma 7 requires the adapter object!
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
