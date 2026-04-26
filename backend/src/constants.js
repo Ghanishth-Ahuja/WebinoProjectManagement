031e095bb5bf69b1c55f4a69972e2d8999572c5b
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-26 23:39:25 +0530

    changed server configs

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,5 +1,5 @@
 export const DBNAME = "projectmgmt";
-export const IS_PRODUCTION_ENVIRONMENT = true;
+export const IS_PRODUCTION_ENVIRONMENT = false;
 export const HOST_NAME = IS_PRODUCTION_ENVIRONMENT
   ? "http://localhost:5173"
   : "https://something.com";
--- backend/src/dbcon/dbConnection.js
+++ backend/src/dbcon/dbConnection.js
@@ -1,29 +1,30 @@
-// import { Pool } from 'pg';
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import { PrismaClient } from '@prisma/client';
-// // import { PrismaClient } from '../../generated/prisma/client';
-// import { IS_PRODUCTION_ENVIRONMENT } from '../constants.js';
-// import "dotenv/config";
-// const connectionString = IS_PRODUCTION_ENVIRONMENT ? process.env.DATABASE_URL : process.env.POOLED_URL;
-// let prisma;
-// if (IS_PRODUCTION_ENVIRONMENT) {
-//     const adapter = new PrismaPg({ connectionString: connectionString });
-//     prisma = new PrismaClient({ adapter });
-// } else {
-//     const pool = new Pool({ connectionString });
-//     const adapter = new PrismaPg(pool);
-//     prisma = new PrismaClient({ adapter });
-// }
-
-
-// export default prisma;
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '../../generated/prisma/client';
+import { IS_PRODUCTION_ENVIRONMENT } from '../constants.js';
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
+const connectionString = IS_PRODUCTION_ENVIRONMENT ? process.env.DATABASE_URL : process.env.POOLED_URL;
+let prisma;
+if (IS_PRODUCTION_ENVIRONMENT) {
+    const adapter = new PrismaPg({ connectionString: connectionString });
+    prisma = new PrismaClient({ adapter });
+} else {
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    prisma = new PrismaClient({ adapter });
+}
 
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaPg({ connectionString: connectionString });
-const prisma = new PrismaClient({ adapter });
 
 export default prisma;
+
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../../generated/prisma/client.js";
+
+// const connectionString = process.env.DATABASE_URL;
+
+// const adapter = new PrismaPg({ connectionString: connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
