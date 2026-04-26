8f1301a446dbf5d47cd075e4641bfbae30b0f732
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-26 23:48:35 +0530

    changed frontend cors domain

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,4 +2,4 @@ export const DBNAME = "projectmgmt";
 export const IS_PRODUCTION_ENVIRONMENT = false;
 export const HOST_NAME = IS_PRODUCTION_ENVIRONMENT
   ? "http://localhost:5173"
-  : "https://something.com";
+  : "https://webinoprojectmanagement.onrender.com";
